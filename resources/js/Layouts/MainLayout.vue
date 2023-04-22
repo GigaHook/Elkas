510b2b05e1350d6bdcd7d90fc141a212decd106d
GigaHook <smirka.n.u@gmail.com>
2023-04-22 14:29:37 +0700

    nav development

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+
+    <v-container style="min-width: 100vw;" class="pa-3">
+      <v-app-bar style="height: 42px;" class="">
+        <v-row>
+          <v-col xl="8" md="9" sm="12" class="py-0">
+            <v-btn>Главная</v-btn>
+            <v-btn class="nav-item h-100">Дэшборд</v-btn>
+            <v-btn class="nav-item">Дэшборд</v-btn>
+            
+          </v-col>
+        </v-row>
+      </v-app-bar>
+    </v-container>
+
+    <v-container style="min-width: 100vw;">
+      <slot/>
+    </v-container>
+
+  </v-app>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+
+
+
+
+</script>
+
+<style scoped>
+.nav-item{
+  width:100px;
+  height: 42px;
+  display: inline-block;
+}
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,38 @@
+<template>
+	<Head title="Home" />
+	<MainLayout>
+
+
+
+		<div>
+			<Link href="/dashboard">Тест</Link>
+		</div>
+
+	</MainLayout>
+</template>
+
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+defineProps({
+	canLogin: {
+		type: Boolean,
+	},
+	canRegister: {
+		type: Boolean,
+	},
+	laravelVersion: {
+		type: String,
+		required: true,
+	},
+	phpVersion: {
+		type: String,
+		required: true,
+	},
+});
+
+
+</script>
+
+<style lang="scss" scoped></style>
